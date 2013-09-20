cc59116660348644de7f43d08bd625a9a5faff61
bmohamed <bmohamedlaval@gmail.com>
2013-09-20 12:56:08 -0400

    update ex03

--- lib/affiche.dart
+++ lib/affiche.dart
@@ -1,15 +1,15 @@
 part of ex03;
 
 
-String afficherLaPremiereReponse(double width, double height, double area) =>
+String afficherLaPremiereReponse(num width, num height, num area) =>
     ('in meter the widht of the rectangle = $width, the height = $height and the area = $area');
 
 
 
-String converstionMetreToKilometre(double width, double height, double area){
-  double x=00.1;
+String converstionMetreToKilometre(num width, num height, num area){
+  num x=00.1;
   return('in kilometre the widht of the rectangle = ${width*x}, the height = ${height*x} and the area = ${area*x}');
 }
 
-String afficherLaDeuxiemeReponse(double distance, double time, double speed) =>   
+String afficherLaDeuxiemeReponse(num distance, num time, num speed) =>   
     ('in meter the distance of the circuit = $distance, the average time = $time and the average speed = $speed');
